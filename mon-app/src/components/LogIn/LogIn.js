49c0e4347e2cf7f5e6958c4c8eb3e6f8b175d891
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2022-06-02 08:42:38 +0200

    Finish login page

--- mon-app/src/components/LogIn/LogIn.js
+++ mon-app/src/components/LogIn/LogIn.js
@@ -5,11 +5,16 @@ export default function LogIn() {
         <div className="login-div">
             <form className="login-form">
                 <h1>Log In</h1>
-                <input required name="pseudo"/>
-                <input required name="password" type="password"/>
-                <input value="Log In" type="submit"/>
-            </form>
+                <input required placeholder="Pseudo" name="pseudo"/>
 
+                <input required placeholder="Password" name="password" type="password"/>
+
+                <input className="btn" value="Sign In" type="submit"/>
+            </form>
+            <div>
+                <h1>New here ?</h1>
+                <button > Register</button>
+            </div>
         </div>
     );
 }
