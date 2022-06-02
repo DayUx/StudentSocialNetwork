3bf7ee933e07d1cef80a8e87466631df629eaeee
Tom <tom.nicol@hotmail.fr>
2022-06-02 08:41:41 +0200

    update json

--- function.js
+++ function.js
@@ -55,7 +55,7 @@ class User {
             }
             }
         });
-        dbo.createCollection("Message", {
+        dbo.createCollection("School", {
             
         bsonType: "object",
         required: ["nom", "description", "users"],
