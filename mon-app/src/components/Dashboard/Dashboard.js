0296863b8b26645d97ad9c0d43808f9529267ec1
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2022-06-16 08:11:24 +0200

    amelioration du back

--- mon-app/src/components/Dashboard/Dashboard.js
+++ mon-app/src/components/Dashboard/Dashboard.js
@@ -3,16 +3,20 @@ import React from 'react';
 
 
 export default function Dashboard() {
+
+    const schools_buttons = [];
+
+    for (let i = 0; i < 10; i++) {
+       schools_buttons.push( <button>School {i}</button>);
+    }
+
+
     return(
 
         <div>
             <nav>
                 <div className="nav-wrapper">
-                    {
-                        for (let i = 0; i < 10; i++) {
-                        <a href="#" className="brand-logo">Logo</a>
-                        }
-                    }
+                    {schools_buttons}
                 </div>
             </nav>
             <div>
